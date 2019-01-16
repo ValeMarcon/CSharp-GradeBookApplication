5a3732bc96ab8139e69bef10b5ea6019c7ec3f24
Valentina Marcon <vavi1996@yahoo.it>
2019-01-16 18:03:48 +0100

    added weight

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,25 +34,27 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-
+            bool weight;
+            weight = Boolean.TryParse(parts[3], out weight);
+               
             BaseGradeBook gradeBook;
             
                 if (type == "standard")
                 {
-                    gradeBook = new StandardGradeBook(name);
+                    gradeBook = new StandardGradeBook(name, weight);
                     Console.WriteLine("Created gradebook {0}.", name);
                     GradeBookUserInterface.CommandLoop(gradeBook);
                 }
                 else if (type == "ranked")
                 {
-                    gradeBook = new RankedGradeBook(name);
+                    gradeBook = new RankedGradeBook(name, weight);
                     Console.WriteLine("Created gradebook {0}.", name);
                     GradeBookUserInterface.CommandLoop(gradeBook);
 
