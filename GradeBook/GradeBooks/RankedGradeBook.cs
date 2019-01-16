9b0d379fccc723eb2a66756883c64cdcb3eb2d80
Valentina Marcon <vavi1996@yahoo.it>
2019-01-16 17:42:51 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,16 +19,33 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException();
             var step = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-            if (grades[step -1] <= averageGrade)
+            if (grades[step - 1] <= averageGrade)
                 return 'A';
-            else if (grades[step*2 - 1] <= averageGrade)
+            else if (grades[step * 2 - 1] <= averageGrade)
                 return 'B';
-            else if (grades[step*3 - 1] <= averageGrade)
+            else if (grades[step * 3 - 1] <= averageGrade)
                 return 'C';
-            else if (grades[step*4 - 1] <= averageGrade)
+            else if (grades[step * 4 - 1] <= averageGrade)
                 return 'D';
             else return 'F';
-            
+
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order " +
+                    "to properly calculate a student's overall grade.");
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order " +
+                    "to properly calculate a student's overall grade.");
+            base.CalculateStudentStatistics(name);
         }
     }
 }
