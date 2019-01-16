096036471d1a133cef16962b09bf238f683f3e4f
Valentina Marcon <vavi1996@yahoo.it>
2019-01-16 17:53:18 +0100

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,9 +40,27 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            var type = parts[2].ToLower();
+
+            BaseGradeBook gradeBook;
+            
+                if (type == "standard")
+                {
+                    gradeBook = new StandardGradeBook(name);
+                    Console.WriteLine("Created gradebook {0}.", name);
+                    GradeBookUserInterface.CommandLoop(gradeBook);
+                }
+                else if (type == "ranked")
+                {
+                    gradeBook = new RankedGradeBook(name);
+                    Console.WriteLine("Created gradebook {0}.", name);
+                    GradeBookUserInterface.CommandLoop(gradeBook);
+
+                }
+            
+            else
+                Console.WriteLine($"{type} is not a supported type of gradebook, please try again");
+
         }
 
         public static void LoadCommand(string command)
